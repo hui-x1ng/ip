2c58d341211e0ed9d05dfe4cd59d67a1cd857925
zichen05 <e1481842@u.nus.edu>
2025-08-22 13:43:50 +0800

    Add Level-1:echo

--- src/main/java/xiaoDu.java
+++ src/main/java/xiaoDu.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class xiaoDu {
     public static void main(String[] args) {
         System.out.println("Hello I'm xiaoDu\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
     }
 }
