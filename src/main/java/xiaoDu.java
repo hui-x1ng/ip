11e146317e8889816e609fb92ff48e17ce82f5ac
zichen05 <e1481842@u.nus.edu>
2025-08-22 15:40:09 +0800

    Add Level-3: Add ability to mark tasks as done/not done

--- src/main/java/xiaoDu.java
+++ src/main/java/xiaoDu.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class xiaoDu {
@@ -5,8 +6,7 @@ public class xiaoDu {
         System.out.println("Hello I'm xiaoDu\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String input;
-        int i =1;
-        String result="";
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             input = scanner.nextLine();
@@ -14,17 +14,45 @@ public class xiaoDu {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")){
-                System.out.println(result);
-            }else {
-                System.out.println("added: "+input);
-                if(result.isEmpty()){
-                    result+=i+". "+input;
-                    i++;
-                }else {
-                    result +="\n"+ i + ". " + input;
-                    i++;
+            } else if (input.equals("list")) {
+                if (tasks.isEmpty()) {
+                    System.out.println("The list is empty.");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
+                    }
                 }
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                        tasks.get(taskNumber).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks.get(taskNumber));
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please provide a valid task number!");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskNumber >= 0 && taskNumber < tasks.size()) {
+                        tasks.get(taskNumber).markAsNotDone();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks.get(taskNumber));
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please provide a valid task number!");
+                }
+            } else {
+                Task newTask = new Task(input);
+                tasks.add(newTask);
+                System.out.println("added: " + input);
             }
         }
 
