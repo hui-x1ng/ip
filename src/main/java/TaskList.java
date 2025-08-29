a085f1aa0a7fe74d1d1a6822d9797369c6032387
zichen05 <e1481842@u.nus.edu>
2025-08-29 13:43:26 +0800

    make the code more OOP

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public boolean isValidIndex(int index) {
+        return index >= 0 && index < tasks.size();
+    }
+}
